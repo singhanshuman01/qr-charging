acf878db423efff58a66b23d3b74fb088eb2491c
singhanshuman01 <singhanshuman592@gmail.com>
2025-03-08 17:31:20 +0530

    added oled display code but not confirmed yet, waiting for 3.3v power supply

--- nodemcu-script/nodemcu-script.ino
+++ nodemcu-script/nodemcu-script.ino
@@ -1,13 +1,34 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WebServer.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-const int relayPin = D1;
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+const int relayPin = D3;
 const char* ssid = "realme8i";
 const char* password = "ansh0098";
-const char* flaskServerURL = "http://192.168.149.21:5000/register_ip";
+const char* flaskServerURL = "http://192.168.150.21:5000/register_ip";
 
 ESP8266WebServer server(80);
+
+void printOled(const char* s){
+  Serial.println(s);
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+  display.println(s);
+  display.display(); 
+}
 
 void announceIP() {
   if (WiFi.status() == WL_CONNECTED) {
@@ -33,6 +54,13 @@ void announceIP() {
 
 void setup() {
   Serial.begin(115200);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3D)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  printOled("Idle...");
+
   pinMode(relayPin, OUTPUT);
   digitalWrite(relayPin, LOW);
 
@@ -52,11 +80,13 @@ void setup() {
   // Define routes
   server.on("/relay_on", []() {
     digitalWrite(relayPin, HIGH);
+    printOled("Charging...");
     server.send(200, "application/json", "{\"status\": \"Charging\"}");
   });
 
   server.on("/relay_off", []() {
     digitalWrite(relayPin, LOW);
+    printOled("Idle...");
     server.send(200, "application/json", "{\"status\": \"Idle\"}");
   });
 
